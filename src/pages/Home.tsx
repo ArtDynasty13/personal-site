597b1a1aac00bb93d182fda67c412c3ab6601860
ArtDynasty13 <arthurwitte18@gmail.com>
2025-08-20 20:23:12 -0600

    fix: update-bio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
             <p> 
                 I am a second year Computing Science Student at the University of Alberta. 
                 I have contributed frontend web development to two startups (focused on <i>artificial intelligence</i> and <i>ergonomic keyboards</i>, respectively)
-                and one neuroscience clinic. 
+                and a neuroscience clinic. 
                 I fell in love with programming through <strong>game development</strong>, and my current interests
                 are creating <strong>websites</strong>, exploring <strong>operating systems</strong> and learning as much as I can about the field.
             </p>
